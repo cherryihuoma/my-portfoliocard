2034e32a5f82a09a950937a4d406a5ed77b56b6c
ÎæmCherry <treasurecherish4life@gmail.com>
2025-09-14 00:09:12 +0100

    Add files via upload

---
+++ PortfolioCard.js
@@ -0,0 +1,19 @@
+function openX() {
+  alert("Opening X...");
+  window.location.href = "https://x.com/cherry_ihuoma";
+}
+
+function openGithub() {
+  alert("Opening GitHub...");
+  window.location.href = "https://github.com/cherryihuoma";
+}
+
+function openLinkedln() {
+  alert("Opening Linkedln....");
+  window.location.href = "https://www.linkedin.com/in/oluwarantimi-orenibi-608719310?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+}
+
+function hireMe() {
+  alert("Let's work together!");
+  window.location.href = "https://mailto:treasurecherish4life@gmail.com";
+}
